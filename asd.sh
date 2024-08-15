f2134f117ee1ef3110d5ec104b46563d69a7aa28
hertjo <herter-johannes@web.de>
2024-08-15 14:30:00 -0400

    Added full project code

--- asd.sh
+++ asd.sh
@@ -4,4 +4,4 @@ COMMIT_DATE="2024-08-15T14:30:00"
 git add .
 GIT_AUTHOR_DATE="$COMMIT_DATE" GIT_COMMITTER_DATE="$COMMIT_DATE" \
 git commit -m "Added full project code"
-git push origin main
+git push origin master
