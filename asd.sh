e070bb6c061234d0909934d95e965db9299220d2
hertjo <herter-johannes@web.de>
2024-08-15 14:30:00 -0400

    Clean SB backward euler

--- asd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-COMMIT_DATE="2024-08-15T14:30:00"
-git add .
-GIT_AUTHOR_DATE="$COMMIT_DATE" GIT_COMMITTER_DATE="$COMMIT_DATE" \
-git commit -m "Added full project code"
-git push origin master
